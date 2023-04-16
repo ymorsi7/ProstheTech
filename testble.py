971a4f3a152862bad92f6595d9518cc0ad937efb
Girish Krishnan <gikrishnan@ucsd.edu>
2023-04-15 23:04:34 -0700

    add averaging for samples

--- testble.py
+++ testble.py
@@ -22,6 +22,42 @@ import RPi.GPIO as GPIO
 import time
 from RpiMotorLib import RpiMotorLib
 
+"""
+A class to provide a circular list structure that maintains its length as elements are added to it.
+"""
+class CircularList(list):
+
+  def __init__(self, data, maxlen=0):
+    assert isinstance(data, list), "Invalid data. It must be a list."
+
+    # zero-pad or trim data from the left to fit to maximum length
+    maxlen = len(data) if(maxlen == 0) else maxlen
+    delta = maxlen - len(data)
+    if(delta > 0):
+      data = [0]*delta + data # prepend zeros
+    else:
+      data[:-maxlen] = []     # trim the leftmost portion
+
+    # initialize the parent list constructor with the data
+    super().__init__(data)
+
+  def add(self, data):
+    # convert to iterable (list) if just a single number
+    data = data if isinstance(data, list) else [data]
+
+
+    # shift the array if the shift is less than array length
+    shift = len(data)
+    maxlen = len(self)
+    if(shift < maxlen):
+      self[:-shift] = self[shift:] # shift the existing data left
+      self[-shift:] = data         # append the new data to the end
+    else:
+      self[:] = data[-maxlen:]     # only copy the rightmost portion
+
+  def clear(self):
+   self.add([0]*len(self))
+
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 logger = logging.getLogger(__name__)
 
@@ -36,6 +72,10 @@ async def generate_random_data(request: Request) -> Iterator[str]:
     """
     client_ip = request.client.host
     logger.info("Client %s connected", client_ip)
+
+    last_50_samples_data1 = CircularList([],maxlen=50)
+    last_50_samples_data2 = CircularList([],maxlen=50)
+
     while True:
         try:
             data = serialPort.readline()
@@ -52,8 +92,20 @@ async def generate_random_data(request: Request) -> Iterator[str]:
             yield f"data:{json_data}\n\n"
         except:
             continue
-        avgdata1 =
-        avgdata2 = 
+        
+        last_50_samples_data1.add(newdata[0])
+        last_50_samples_data2.add(newdata[1])
+
+        # Get average of last 50 samples
+        avg_data1 = sum(last_50_samples_data1) / len(last_50_samples_data1)
+        avg_data2 = sum(last_50_samples_data2) / len(last_50_samples_data2)
+
+        # Get the difference between the average and the current value
+        diff_data1 = avg_data1 - float(newdata[0])
+        diff_data2 = avg_data2 - float(newdata[1])
+
+        # print the difference
+
         await asyncio.sleep(0.1)
 
 async def move_left():
