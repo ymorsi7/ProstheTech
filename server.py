dab428ca4cfdcfb2a7bf5c975ed4c5f0b6e1f388
Yusuf Morsi <ymorsi7@gmail.com>
2023-04-16 08:24:54 -0700

    file stuff

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-# Necessary Imports
 from fastapi.responses import RedirectResponse
 from fastapi import FastAPI                   # The main FastAPI import
 from fastapi.responses import HTMLResponse    # Used for returning HTML responses
@@ -13,9 +12,15 @@ app = FastAPI()
 # Mount static directories
 app.mount("/public", StaticFiles(directory="public"), name="public")
 app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+
 @app.get("/", response_class = HTMLResponse)
 def get_html() -> HTMLResponse:
     with open("index.html") as html:
+        return HTMLResponse(content=html.read())
+    
+@app.get("/demo", response_class = HTMLResponse)
+def get_html() -> HTMLResponse:
+    with open("demo.html") as html:
         return HTMLResponse(content=html.read())
 
 if __name__ == "__main__":
